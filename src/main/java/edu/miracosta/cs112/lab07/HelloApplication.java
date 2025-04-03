96f210d25c34098498827ac1f7d67c46ed0010b4
David Charles Q <davidcharlesquinto@gmail.com>
2025-04-03 12:40:28 -0700

    Did Steps 1-18 of the lab.

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -1,13 +1,15 @@
 package edu.miracosta.cs112.lab07;//package name here depending on your IDE
 
 import javafx.application.Application;  //abstract class used for JavaFX GUI's
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;              //class for GUI window
 import javafx.scene.Scene;              //class for specific view in GUI window
 import javafx.scene.layout.VBox;        //class for layout pane, organized top-to-bottom
 import javafx.scene.control.Label;      //class for label component
 import javafx.scene.control.Button;     //class for button component
 import javafx.event.EventHandler;       //interface for handling events
-import javafx.event.ActionEvent;        //class for type of event for action (like button or key pressed)
+import javafx.event.ActionEvent;        //class for type of event for action (like button or key press
 
 public class HelloApplication extends Application  { //inheriting core functionality + this class will handle events
     /*** GUI COMPONENTS ***/
@@ -19,8 +21,26 @@ public class HelloApplication extends Application  { //inheriting core functiona
     }
 
     // TODO: follow steps 2-9 in README.md to create a start method
+    public void start (Stage primaryStage) {
+        primaryStage.setTitle("Hello GUI: I am David Quinto.");
 
-    // TODO: follow steps 10-21 in README.md to add objects to your layout (inside start)
+        StackPane layout = new StackPane();
 
-    // TODO: follow steps 22-34 in README.md to create an event handler
+        Scene scene = new Scene(layout, 300, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        // TODO: follow steps 10-21 in README.md to add objects to your layout (inside start)
+        Label label = new Label();
+        label.setText("Hello GUI World!");
+        layout.getChildren().add(label);
+
+        AnchorPane anchorPane = new AnchorPane();
+        anchorPane.setBottomAnchor(label, 0.0);
+        anchorPane.setRightAnchor(label,10.0);
+        anchorPane.setTopAnchor(label, 10.0);
+        anchorPane.setLeftAnchor(label, 0.0);
+        anchorPane.getChildren().add(label);
+        layout.getChildren().add(anchorPane);
+        // TODO: follow steps 22-34 in README.md to create an event handler
+    }
 }
